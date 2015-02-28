c0a595694441384265ff5cc7ed290dd48c8f4a37
Sally Kong <kongsally94@gmail.com>
2015-02-28 13:28:35 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2014 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var express = require('express'),
+  app = express(),
+  bluemix = require('./config/bluemix'),
+  watson = require('watson-developer-cloud-alpha'),
+  extend = require('util')._extend,
+  fs = require('fs'),
+  dummy_text = fs.readFileSync('sample.txt');
+
+
+// Bootstrap application settings
+require('./config/express')(app);
+
+// if bluemix credentials exists, then override local
+var credentials = extend({
+    version: 'v2',
+    url: 'https://gateway.watsonplatform.net/personality-insights/api',
+    username: 'f16f1b5a-0939-4889-81fb-99a11e7f53e5',
+    password: '9QIIY1yDmK6W'
+}, bluemix.getServiceCreds('personality_insights')); // VCAP_SERVICES
+
+// Create the service wrapper
+var personalityInsights = new watson.personality_insights(credentials);
+
+// render index page
+app.get('/', function(req, res) {
+  res.render('index', { content: dummy_text });
+});
+
+app.post('/', function(req, res) {
+  personalityInsights.profile(req.body, function(err, profile) {
+    if (err) {
+      if (err.message){
+        err = { error: err.message };
+      }
+      return res.status(err.code || 500).json(err || 'Error processing the request');
+    }
+    else
+      return res.json(profile);
+  });
+});
+
+var port = process.env.VCAP_APP_PORT || 3000;
+app.listen(port);
+console.log('listening at:', port);
